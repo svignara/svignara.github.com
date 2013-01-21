495540ca7a6a8cd9fcc27e81fcabb36cf1bfe9ff
Suvethan Vignarajah <svignarajah@sapient.com>
2013-01-21 01:28:12 -0500

    added about me section

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,3 +1,19 @@
 function ProjectEntriesCtrl($scope) {
   $scope.projects = projects;
 }
+
+function NetworksCtrl($scope) {
+  $scope.networks = networks;
+}
+
+function SkillsCtrl($scope) {
+  $scope.skills = skills;
+}
+
+function BioCtrl ($scope) {
+  $scope.bio = bio;
+}
+
+function ExperiencesCtrl ($scope) {
+  $scope.experiences = experiences;
+}
