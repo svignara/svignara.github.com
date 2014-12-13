f72e0c6ba12a3503e2f98ce362361f6395819a05
svignara <suvethan@gmail.com>
2014-12-13 00:39:56 -0500

    minor updates

--- bucketlist/js/main.js
+++ bucketlist/js/main.js
@@ -2,13 +2,22 @@
 
     var bucketlist = {
         init : function(){
-            this.initSignupUI();
+            this.initUI();
+        },
+        initUI : function(){
+            var self = this;
+            $(function(){
+                self.initSignupUI();
+                self.initLoginUI();
+            });
         },
         initSignupUI : function(){
             var self = this;
-            $(function(){
-                self.signupFormSubmitListener();
-            });
+            self.signupFormSubmitListener();
+        },
+        initLoginUI : function(){
+            var self = this;
+            self.loginFormSubmitListener();
         },
         signupFormSubmitListener : function(){
             $('form[name="signup"]').on('submit', function(evt){
@@ -20,6 +29,22 @@
                 window.localStorage.setItem('user', JSON.stringify(user));
                 $('body').addClass('signedup');
             });
+        },
+        loginFormSubmitListener : function(){
+            var self = this;
+            $('form[name="login"]').on('submit', function(evt){
+                evt.preventDefault();
+                if (!self.validUser($('#inputEmail').val(), $('#inputPassword').val())){
+                    $(this).hasClass('has-error');
+                    return false;
+                }else{
+                    window.location.href = "bucketlist.html";
+                }
+            });
+        },
+        validUser : function(email, pass){
+            var user = (window.localStorage.getItem('user')) ? JSON.parse(window.localStorage.getItem('user')) : { email : "", pass : "" };
+            return (email === user.email && pass === user.pass);
         }
     }
 
