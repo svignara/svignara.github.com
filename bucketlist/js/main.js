ad501cb69840c25ee53b67566599e1004f7af746
svignara <suvethan@gmail.com>
2014-12-13 00:00:09 -0500

    minor updates

--- bucketlist/js/main.js
+++ bucketlist/js/main.js
@@ -1,23 +1,19 @@
-(function($){
-
-    var bucketlist = {
-        init : function(){
-            this.initSignupUI();
-        },
-        initSignupUI : function(){
-            var self = this;
-            $(function(){
-                self.signupFormSubmitListener();
-            });
-        },
-        signupFormSubmitListener : function(){
-            $('form[name="signup"]').on('submit', function(evt){
-                evt.preventDefault();
-                console.log('submit data');
-            });
-        }
+var Bucketlist = {
+    init : function(){
+        this.initSignupUI();
+    },
+    initSignupUI : function(){
+        var self = this;
+        $(function(){
+            self.signupFormSubmitListener();
+        });
+    },
+    signupFormSubmitListener : function(){
+        $('form[name="signup"]').on('submit', function(evt){
+            evt.preventDefault();
+            console.log('submit data');
+        });
     }
+}
 
-    bucketlist.init();
-
-})(jQuery);
+Bucketlist.init();
