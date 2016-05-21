5d5731f99bfc7cd5bfa506eec24ecde42cc4945e
Suvethan Vignarajah <suvethanvignarajah@oracle.local>
2016-05-21 02:10:03 -0400

    speed force in progress

--- resources/homemade/scripts/main.js
+++ resources/homemade/scripts/main.js
@@ -67,6 +67,9 @@ var SpeedForce = (function(){
 
 			stopBtn.addEventListener('click', function(){
 				navigator.geolocation.clearWatch(watchID);
+				var output = document.createElement('p');
+				output.innerHTML = '---Done---';
+				resultsEl.appendChild(output);
 			});
 		}
 
